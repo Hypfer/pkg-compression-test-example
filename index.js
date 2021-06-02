05e58eb2105753c4ee19d763f0da4b0a31e82a0a
Sören Beye <github@hypfer.de>
2021-06-02 23:45:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api/lol', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use("/", express.static(path.join(__dirname, "./", "res")))
+
+app.use((err, req, res, next) => {
+    console.log("Unhandled WebServer Error", err);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
