da9363e3f59b751709066d91a1d6136313a233ea
Sören Beye <github@hypfer.de>
2021-06-05 19:32:32 +0200

    Updated example

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/api/lol', (req, res) => {
+app.get('/api/foo', (req, res) => {
     res.send('Hello World!')
 })
 
